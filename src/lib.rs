7a2742efabd2f164d866bce2d5c983fe97029f68
JoseVitorNobre <62249331+JoseVitorNobre@users.noreply.github.com>
2024-11-29 19:47:01 +0000

    adicionei modularização

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,4 @@
-pub mod front_of_house {
-    
-    pub mod hosting {
-        pub fn add_to_wait_list(){
-            println!("Added to wait list!");
-        }
-        pub fn seat_at_table(){
-            println!("Sat at the table!");
-        }
-    }
+pub mod front_of_house;
+pub mod back_of_house;
 
-    pub mod serving {
-        pub fn take_order() {}
-
-        pub fn serve_order() {}
-
-        pub fn take_payment() {}
-    }
-}
-
-pub mod back_of_house {
-    pub fn take_care_trash(){
-        println!("Taking care of the trash!");
-    }
-}
-
-pub const PI:f32 = 3.14;
+pub const PI: f32 = 3.14;
